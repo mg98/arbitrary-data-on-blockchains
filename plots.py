e639956416b27e9ec690eb13264ec939f1cb5646
Marcel Gregoriadis <marcel.gregoriadis@informatik.hu-berlin.de>
2021-10-28 12:03:11 +0100

    renamed axis

--- plots.py
+++ plots.py
@@ -1,6 +1,7 @@
 import sqlite3
 import matplotlib.pyplot as plt
 from dateutil import parser
+import numpy as np
 
 def get_plot_data(query: str, date = True) -> tuple[list[str], list[any]]:
 	cursor = conn.execute(query)
@@ -18,7 +19,7 @@ conn = sqlite3.connect("results.db")
 
 axs[0, 0].set_title("Frequency of Text Transactions")
 axs[0, 0].plot_date(*get_plot_data("SELECT strftime('%Y-%m', block_timestamp) AS dt, COUNT(*) AS c FROM text_results GROUP BY dt"), '-')
-axs[0, 0].set(xlabel="Time",ylabel="Amount")
+axs[0, 0].set(xlabel="Time",ylabel="Transaction amount per month")
 
 axs[0, 1].set_title("Frequency of Text Length")
 axs[0, 1].plot(*get_plot_data("""
@@ -26,16 +27,16 @@ SELECT LENGTH(data) AS data_length, COUNT(*) AS c
 FROM text_results
 GROUP BY data_length
 """, False), '-')
-axs[0, 1].set(xlabel="Text length in characters",ylabel="Amount")
+axs[0, 1].set(xlabel="Text length in characters", ylabel="Transaction amount")
 axs[0, 1].set_xscale('log')
 
 axs[1, 0].set_title("Frequency of Files Transactions")
-axs[1, 0].plot_date(*get_plot_data("SELECT strftime('%Y-%m', block_timestamp) AS dt, COUNT(*) AS c FROM files_results GROUP BY dt"), '-')
-axs[1, 0].set(xlabel="Time",ylabel="Amount")
+axs[1, 0].plot_date(*get_plot_data("SELECT strftime('%Y-%m', block_timestamp) AS dt, COUNT(*) AS c FROM files_results WHERE deleted = 0 GROUP BY dt"), '-')
+axs[1, 0].set(xlabel="Time",ylabel="Transaction amount per month")
 
 axs[1, 1].set_title("Frequency of Files Transactions (Embedded Only)")
-axs[1, 1].plot_date(*get_plot_data("SELECT strftime('%Y-%m', block_timestamp) AS dt, COUNT(*) AS c FROM files_results WHERE method = 'Embedded' GROUP BY dt"), '-')
-axs[1, 1].set(xlabel="Time",ylabel="Amount")
+axs[1, 1].plot_date(*get_plot_data("SELECT strftime('%Y-%m', block_timestamp) AS dt, COUNT(*) AS c FROM files_results WHERE method = 'Embedded' AND deleted = 0 GROUP BY dt"), '-')
+axs[1, 1].set(xlabel="Time",ylabel="Transaction amount per month")
 
 conn.close()
 plt.tight_layout(pad=1)
